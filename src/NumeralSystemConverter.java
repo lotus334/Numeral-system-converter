c9c35bf6e0731a2abea536b5dc3e2b722c13e290
Васильев Дмитрий <lotus33497@yandex.ru>
2020-09-08 09:50:31 +0700

    convert decimals to binary, octal and hexadecimal

--- src/NumeralSystemConverter.java
+++ src/NumeralSystemConverter.java
@@ -7,7 +7,7 @@ public class NumeralSystemConverter {
         int num = scanner.nextInt();
         int maxpowersOfTwo = 1;
         int lenghtOfList = 1;
-        int base = 8;
+        int base = scanner.nextInt();
         while (maxpowersOfTwo < num) {
             maxpowersOfTwo *= base;
             lenghtOfList++;
@@ -27,7 +27,46 @@ public class NumeralSystemConverter {
                 ans[i] = 0;
             }
         }
-
-        System.out.println(ans[lenghtOfList - 1]);
+        boolean startWithZero = true;
+        if (base == 16) {
+            System.out.print("0x");
+            for (int i = 0; i < lenghtOfList; i++) {
+                if (ans[i] == 0 && startWithZero) {
+                    continue;
+                }
+                if (ans[i] == 10) {
+                    System.out.print("a");
+                } else if (ans[i] == 11) {
+                    System.out.print("b");
+                } else if (ans[i] == 12) {
+                    System.out.print("c");
+                } else if (ans[i] == 13) {
+                    System.out.print("d");
+                } else if (ans[i] == 14) {
+                    System.out.print("e");
+                } else if (ans[i] == 15) {
+                    System.out.print("f");
+                } else {
+                    System.out.print(ans[i]);
+                }
+                startWithZero = false;
+            }
+        } else {
+            if (base == 8) {
+                System.out.print("0");
+            } else if (base == 2) {
+                System.out.print("0b");
+            }
+            for (int i = 0; i < lenghtOfList; i++) {
+                if (ans[i] == 0 && startWithZero) {
+                    continue;
+                }
+                System.out.print(ans[i]);
+                startWithZero = false;
+            }
+        }
+        if (startWithZero) {
+            System.out.print("0");
+        }
     }
 }
