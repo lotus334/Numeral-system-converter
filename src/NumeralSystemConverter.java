494bc7e8bfab44f2005f164db2c119dc4dda23cb
Васильев Дмитрий <lotus33497@yandex.ru>
2020-09-08 10:33:32 +0700

    Almost full converter from any to any radix

--- src/NumeralSystemConverter.java
+++ src/NumeralSystemConverter.java
@@ -4,21 +4,22 @@ import java.util.Scanner;
 public class NumeralSystemConverter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        long num = scanner.nextLong();
-        int radix = scanner.nextInt();
-        switch (radix) {
-            case 2:
-                System.out.print("0b");
-                break;
-            case 8:
-                System.out.print("0");
-                break;
-            case 16:
-                System.out.print("0x");
-                break;
-            default:
-                break;
+        int sourceRadix = scanner.nextInt();
+        long sourceNum = scanner.nextLong();
+        int targetRadix = scanner.nextInt();
+        if (sourceRadix != 1 && targetRadix != 1) {
+            long numDecimal = Long.parseLong(String.valueOf(sourceNum), sourceRadix);
+            String numTarget = Long.toString(numDecimal, targetRadix);
+            System.out.println(numTarget);
+        } else if (sourceRadix == 1) {
+            long numDecimal = String.valueOf(sourceNum).length();
+            String numTarget = Long.toString(numDecimal, targetRadix);
+            System.out.println(numTarget);
+        } else if (targetRadix == 1) {
+            long numDecimal = Long.parseLong(String.valueOf(sourceNum), sourceRadix);
+            for (int i = (int) numDecimal; i > 0; i--) {
+                System.out.print(1);
+            }
         }
-        System.out.print(Long.toString(num, radix));
     }
 }
